64a01d951b80d5f53a77c2927f9037563dd3f847
Tom5521 <thomas.bored5521@gmail.com>
2024-09-21 15:23:37 -0400

    Update dots

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -87,6 +87,7 @@ return {
 		"loctvl842/monokai-pro.nvim",
 		config = function()
 			require("monokai-pro").setup({
+				transparent_background = false,
 				filter = "ristretto",
 				devicons = true,
 				styles = {
