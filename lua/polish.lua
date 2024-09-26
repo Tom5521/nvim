e162672422ba32774128ec902afb14fd90ec52f6
Tom5521 <thomas.bored5521@gmail.com>
2024-09-26 19:27:58 -0400

    Fix polish.lua

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,6 +1,6 @@
 local arch = require("jit").arch
 
-if arch ~= "arm" or "arm64" then
+if arch == "arm" or arch == "arm64" then
 	print("You are on an ARM system, many of the functions are not going to work here...")
 	-- TODO: Fix this shit
 end
