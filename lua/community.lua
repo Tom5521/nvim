aaab56aa6cea43d78c27913eab9a95cf2ad30066
Tom5521 <thomas.bored5521@gmail.com>
2024-10-11 16:34:45 -0300

    Add java community pack

--- lua/community.lua
+++ lua/community.lua
@@ -15,6 +15,7 @@ return {
 	{ import = "astrocommunity.pack.yaml" },
 	{ import = "astrocommunity.pack.bash" },
 	{ import = "astrocommunity.pack.sql" },
+	{ import = "astrocommunity.pack.java" },
 
 	--INFO: COLORSHEMES
 	{ import = "astrocommunity.colorscheme.monokai-pro-nvim" },
