20e8e186ad30cc76eb7e9a9349a94fe2f8931ded
Tom5521 <thomas.bored5521@gmail.com>
2025-01-28 01:02:12 -0300

    Update nvim

--- lua/polish.lua
+++ lua/polish.lua
@@ -6,13 +6,13 @@ if arch == ("arm" or "arm64") then
 end
 
 if vim.g.neovide then
-	vim.o.guifont = "FiraCode Nerd Font,JetBrains Mono:h12:h12"
-	vim.g.neovide_scale_factor = 0.9
+	vim.o.guifont = "FiraCode Nerd Font,JetBrains Mono:h11:h11"
+	vim.g.neovide_scale_factor = 1
 	vim.g.neovide_confirm_quit = true
 	vim.g.neovide_remember_window_size = true
 	vim.g.neovide_cursor_smooth_blink = true
 	vim.g.neovide_cursor_vfx_mode = "railgun"
 	vim.g.neovide_hide_mouse_when_typing = false
-	vim.g.neovide_cursor_trail_size = 1
+	vim.g.neovide_cursor_trail_size = 0.9
 	vim.g.neovide_cursor_vfx_particle_density = 9.0
 end
